fb639367da91e86254d4138ed674f2c5cc0f5185
Kaoru <kaoru@gmail.com>
2023-12-27 22:27:27 +0800

    guess.py 完成部分邏輯

--- guess.py
+++ guess.py
@@ -1,4 +1,13 @@
 import random
 
 x=random.randint(1,50)
-print(x)
+for i in range(5):
+    print(x)
+    guess=int(input("請猜1-50:"))
+    if guess==x:
+        print("猜對了")
+        break
+
+    else:
+        print("猜錯了")
+
