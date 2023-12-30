495d4ab223ab826d1a0c4ae0bc3d4622009f14e5
Kaoru <kaoru@gmail.com>
2023-12-31 06:04:36 +0800

    guess_my.py大致完成，有刪除不必要檔案

--- guess_my.py
+++ guess_my.py
@@ -1,12 +1,20 @@
 import random
 
-ans = random.randint(1, 100)
+start = int(input("請輸入起始數字:"))
+end = int(input("請輸入中止數字:"))
 
+ans = random.randint(start, end)
 while True:
-    guess = int(input("請輸入猜的數字:"))
+    guess = int(input(f"請輸入猜的數字:"))
     if guess > ans:
+        end = guess
         print("猜小一點")
+        print(f"答案介於{start}-{end-1}")
+
     elif guess < ans:
+        start = guess
         print("猜大一點")
+        print(f"答案介於{start+1}-{end}")
     else:
         print(f"答對了答案是{ans}")
+        break
