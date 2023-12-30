7122439124c987ff05a17709d63dd88add09e403
Kaoru <kaoru@gmail.com>
2023-12-31 06:37:38 +0800

    完成guess_my.py撰寫

--- guess_my.py
+++ guess_my.py
@@ -1,21 +1,24 @@
 import random
 
-start = int(input("請輸入起始數字:"))
-end = int(input("請輸入中止數字:"))
+try:
+    start = int(input("請輸入起始數字:"))
+    end = int(input("請輸入中止數字:"))
 
-ans = random.randint(start, end)
-while True:
-    guess = int(input(f"請猜{start}-{end}的數字:"))
-    if guess > ans:
-        print("猜小一點")
-        if end > guess:
-            end = guess - 1
+    ans = random.randint(start, end)
+    while True:
+        guess = int(input(f"請猜{start}-{end}的數字:"))
+        if guess > ans:
+            print("猜小一點")
+            if end > guess:
+                end = guess - 1
 
-    elif guess < ans:
-        print("猜大一點")
-        if start < guess:
-            start = guess + 1
+        elif guess < ans:
+            print("猜大一點")
+            if start < guess:
+                start = guess + 1
 
-    else:
-        print(f"答對了答案是{ans}")
-        break
+        else:
+            print(f"答對了答案是{ans}")
+            break
+except Exception as e:
+    print(f"錯誤:{e}")
