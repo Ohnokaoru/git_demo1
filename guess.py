037161cf5a02fa1b51e657d8b5e218d0911474dd
Kaoru <kaoru@gmail.com>
2023-12-30 19:29:23 +0800

    完成guess.py

--- guess.py
+++ guess.py
@@ -1,11 +1,12 @@
 import random
 
-x = random.randint(1, 50)
-
 start = 1
 end = 100
+
+x = random.randint(start, end)
+
+
 for i in range(5):
-    print(x)
     y = int(input(f"請猜{start}-{end}:"))
     if y == x:
         print("猜對了")
@@ -14,6 +15,14 @@ for i in range(5):
     else:
         if y > x:
             print("猜低一點")
+            if end > y:
+                end = y - 1
         else:
-            print("猜高一點")
-            print("測試分支")
+            if start < y:
+                start = y + 1
+                print("猜高一點")
+
+if y != x:
+    print(f"答案為{x}")
+else:
+    print(f"猜對了，一共猜{i+1}次")
